75aef0643cb6a59af574d4cc2241cf1fa4b21a1e
Minh Nhat <nhat511111@gmail.com>
2024-10-17 11:12:53 +0700

    Landing page

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import heroImage from '../../images/hero/hero.jpg';
 const Hero = () => {
     return (
         <div id="hero" className="relative w-full h-screen text-center text-white">
-            <img src="src/images/hero/hero.jpg" alt="" className="w-full h-full object-cover" />
+            <img src={heroImage} alt="" className="w-full h-full object-cover" />
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-shadow-md">
                 <h1 className="text-4xl font-bold">Welcome to NhàHàng</h1>
                 <p className="text-xl">Your adventure starts here</p>
